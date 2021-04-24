80404470589b7c04a3e7b794bef7d5e8f8cba6f9
supratim <supratim@elearnmarkets.com>
2021-04-24 22:34:45 +0530

    Basic setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView>
+    <Text>Hello</Text>
+  </SafeAreaView>;
+};
+
+export default App;
