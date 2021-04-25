1241e8863448ed8a76a03561b4fd24865333865d
Goutam Das <goutamdas676@gmail.com>
2021-04-25 15:07:34 +0530

    OnBoarding Screen

---
+++ src/screens/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {View, Text} from 'react-native';
+
+const OnBoarding: FC = () => {
+  return (
+    <View>
+      <Text>OnBoarding</Text>
+    </View>
+  );
+};
+
+export default OnBoarding;
---
+++ src/screens/index.ts
@@ -0,0 +1,2 @@
+export {default as OnBoarding} from './OnBoarding/OnBoarding';
+// https://www.youtube.com/watch?v=1XP28xVToho
